20491900d20b21db4e6d6e5439771adecb8be057
Sweattowel <122240183+Sweattowel@users.noreply.github.com>
2024-12-23 22:35:32 +1000

    Account data and settings template begun and reached Level, Will now need to first optimize it, and ensure that it does what i want

--- src/Pages/Account-and-billing/AccountAndBilling.js
+++ src/Pages/Account-and-billing/AccountAndBilling.js
@@ -10,10 +10,21 @@ const DummyAccount = {
     AccountName: "John Doe",
     AccountEmail: "johndoe@example.com",
     AccountID: 101,
+    AccountPassword: "TEST",
     AccountStartDate: new Date("2022-01-15"),
     AccountBillingPeriod: "Monthly",
     AccountLastBillDate: new Date("2024-11-01"),
     AccountComingBillDate: new Date("2024-12-01"),
+    AccountPriorBillings: [
+        { Amount: 9.99, Date: new Date("2024-11-01"), Status: "Pending" },
+        { Amount: 9.99, Date: new Date("2024-10-01"), Status: "Paid" },
+        { Amount: 9.99, Date: new Date("2024-09-01"), Status: "Paid" },
+    ],
+    SubScriptionDetails: {
+        CurrentPlan: "General",
+        AutoRenew: false,
+        FollowUpDate: new Date("2026-01-01")
+    },    
     AccountDiscrepency: {
         Count: 7,
         DiscrepencyDates: [
@@ -26,7 +37,43 @@ const DummyAccount = {
             new Date("1/12/2015"),
         ]
     },
-    AccountPriorBillings: 35
+    AccountSettings: {
+        SelectedTheme: "Light",
+        PushAdvertisement: false,
+        Active: true,
+        NotificationPreferences: {
+            Email: true,
+            SMS: false,
+            PushNotifications: true
+        },
+        PrivacySettings: {
+            SensitiveData: true,
+            DataShare: false
+        }
+    },
+    LastLoginDate: null,
+    CountLogins: 0,
+    Security: {
+        TwoFactorAuthentication: true,
+        LoginAlert: true,
+        SecurityQuestions: [
+            { Question: "What was your first pet's name?", Answer: "Fluffy" },
+            { Question: "What city were you born in?", Answer: "New York" },
+        ],
+        RegisteredLoginLocations: [
+            {
+                HardWare: "JohnDoePC", IP: "192.168.0.1" ,LoginCount: 2
+            }
+        ]
+    },
+    LanguageAndLocation: {
+      Language: "English",
+      TimeZone: "Etc/GMT-16"  
+    },
+    PriorSupport: [
+        { TicketID: 13489412, Issue: "Overcharged", CustomerSupportOfficer: "Jill Jane", CustomerSupportOfficerID: 3141245, TicketStatus: "Resolved"}
+    ]
+
 };
 
 
