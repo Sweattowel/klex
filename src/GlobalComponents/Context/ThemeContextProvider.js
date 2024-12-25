a0114fa72d1f629124618dc962ea37627ff95826
Sweattowel <122240183+Sweattowel@users.noreply.github.com>
2024-12-25 19:29:34 +1000

    Removed Caching of theme in local storage, instead opted to include it and call from server

--- src/GlobalComponents/Context/ThemeContextProvider.js
+++ src/GlobalComponents/Context/ThemeContextProvider.js
@@ -6,16 +6,6 @@ export const ThemeContextProvider = ({ children } ) => {
     const [theme, setTheme] = useState('');
     const [themeAlt, setThemeAlt] = useState('');
 
-    useEffect(() => {
-        //localStorage.setItem("KLEXTHEMECACHE", JSON.stringify({theme, themeAlt}));
-    },[theme, themeAlt])
-    useEffect(() => {
-        const cachedTheme = JSON.parse(localStorage.getItem("KLEXTHEMECACHE"));
-        console.log(cachedTheme)
-        console.log(cachedTheme["theme"])
-        setTheme(cachedTheme["theme"]);
-        setThemeAlt(cachedTheme["themeAlt"]);
-    },[])
     return (
         <ThemeContext.Provider value={{theme, setTheme, themeAlt, setThemeAlt}}>
             {children}
